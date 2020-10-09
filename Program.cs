48bffe3d808a05669647692d4deaabc4b4ff2522
YaroslawGarbarenko <72256923+YaroslawGarbarenko@users.noreply.github.com>
2020-10-09 18:24:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lb4task1
+{ 
+  class ArrayExample
+  {
+    static void Main()
+    {
+        char[] letters = { 'y', 'a', 'r', 'o', 's', 'l', 'a', 'w', ' ', 'g', 'a', 'r', 'b', 'a', 'r', 'e', 'n', 'k', 'o' };
+        string name = "";
+        int[] a = new int[19];
+        for (int i = 0; i < letters.Length; i++)
+        {
+            name += letters[i];
+            a[i] = i + 1;
+            SendMessage(name, a[i]);
+        }
+        Console.ReadKey();
+    }
+    static void SendMessage(string name, int msg)
+    {
+        Console.WriteLine("Hello, " + name + "! Count to " + msg);
+    }
+  }
+}
